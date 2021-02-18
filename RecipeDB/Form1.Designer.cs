0428288338428c678363c83139eff8d5c202caf2
RolandStrod <KhaleAldeAsad@gmail.com>
2021-02-18 12:09:15 +0200

    red

---
+++ RecipeDB/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace RecipeDB
+{
+    partial class CookBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listRecipe = new System.Windows.Forms.ListBox();
+            this.Recipe = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listIngredient = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listRecipe
+            // 
+            this.listRecipe.FormattingEnabled = true;
+            this.listRecipe.ItemHeight = 16;
+            this.listRecipe.Location = new System.Drawing.Point(58, 77);
+            this.listRecipe.Name = "listRecipe";
+            this.listRecipe.Size = new System.Drawing.Size(151, 148);
+            this.listRecipe.TabIndex = 0;
+            this.listRecipe.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Recipe
+            // 
+            this.Recipe.AutoSize = true;
+            this.Recipe.Location = new System.Drawing.Point(55, 57);
+            this.Recipe.Name = "Recipe";
+            this.Recipe.Size = new System.Drawing.Size(52, 17);
+            this.Recipe.TabIndex = 1;
+            this.Recipe.Text = "Recipe";
+            this.Recipe.Click += new System.EventHandler(this.Recipe_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // listIngredient
+            // 
+            this.listIngredient.FormattingEnabled = true;
+            this.listIngredient.ItemHeight = 16;
+            this.listIngredient.Location = new System.Drawing.Point(250, 77);
+            this.listIngredient.Name = "listIngredient";
+            this.listIngredient.Size = new System.Drawing.Size(148, 148);
+            this.listIngredient.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingredients";
+            // 
+            // CookBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listIngredient);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Recipe);
+            this.Controls.Add(this.listRecipe);
+            this.Name = "CookBook";
+            this.Text = "CookBook";
+            this.Load += new System.EventHandler(this.CookBook_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listRecipe;
+        private System.Windows.Forms.Label Recipe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listIngredient;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RecipeDB/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace RecipeDB
+{
+    public partial class CookBook : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        public CookBook()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["RecipeDB.Properties.Settings.RecipeDBConnectionString"].ConnectionString;
+        }
+
+       
+
+        private void Recipe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CookBook_Load(object sender, EventArgs e)
+        {
+            PopulateRecipes();
+        }
+
+        private void PopulateRecipes()
+        {
+            using(connection = new SqlConnection(connectionString))
+            using(SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
+            {
+                DataTable recipeTable = new DataTable();
+                adapter.Fill(recipeTable);
+
+                listRecipe.DisplayMember = "Name";
+                listRecipe.ValueMember = "Id";
+                listRecipe.DataSource = recipeTable;
+            }
+        }
+
+        private void PopulateIngredients()
+        {
+            string query = "SELECT Recipe.Name, Ingredient.Name FROM Recipe JOIN RecipeIngredient ON Recipe.Id = RecipeIngredient.RecipeId JOIN Ingredient ON Ingredient.Id = RecipeIngredient.IngredientId";
+
+
+
+
+            using (connection = new SqlConnection(connectionString))
+            using(SqlCommand command = new SqlCommand(query, connection))
+            using(SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                //get recipe id from the listRecipe listBox
+                command.Parameters.AddWithValue("@RecipeId", listRecipe.SelectedValue);
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+
+                listIngredient.DisplayMember = "Name";
+                listIngredient.ValueMember = "Id";
+                listIngredient.DataSource = ingredientTable;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateIngredients();
+        }
+    }
+}
